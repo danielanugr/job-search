b7c5e524d8a34a4aa790f89d28d973a2d28b0f7c
Daniel Adhi Nugraha` <danielanugr@yahoo.co.id>
2022-07-11 18:10:29 +0700

    add nav items and the unit test

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,20 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Daniel Corp");
   });
+
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Daniel Corps",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
