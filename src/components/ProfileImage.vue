e4cea8ba3e60749316aa915ab5c05df540b10f81
Daniel Adhi Nugraha` <danielanugr@yahoo.co.id>
2022-07-11 21:16:05 +0700

    add login button and methods

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imgLink"
+    alt="Profile Image"
+    class="w-8 h-8 object-contain rounded-3xl"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imgLink:
+        "https://e7.pngegg.com/pngimages/428/798/png-clipart-link-the-legend-of-zelda-the-wind-waker-cartoon-the-legend-of-zelda-smiley-fictional-character.png",
+    };
+  },
+};
+</script>
